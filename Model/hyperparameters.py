0f8a7b27a322bceaa75a4095c8e6f515abe9e4be
melaniasala <melania.sala@mail.polimi.it>
2024-07-15 00:47:59 +0200

    Add hyperparameters file

---
+++ Model/hyperparameters.py
@@ -0,0 +1,12 @@
+HYPERPARAMETERS = {
+    'model': {
+        'window': 4, # corresponds to the max sequence length allowed (number of temporal steps the model can look back)
+        # 'input_size': 10,
+        # 'hidden_size': 128,
+        # 'output_size': 10
+    },
+    'training': {
+        'lr': 0.001,
+        'epochs': 10
+    }
+}
