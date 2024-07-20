802a02ba2fcf1a83371765696db8c00079b16a7e
melaniasala <melania.sala@mail.polimi.it>
2024-07-21 01:20:37 +0200

    Refactor QuantumCircuitGenerator to include torch_geometric data and update node features and adjacency matrix.
    Still have to deal with batched data. For now focus on single sample in single_sample_QCGRNN.py.

--- Model/QCGRNN.py
+++ Model/QCGRNN.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from torch_geometric.data import Data
 from Data.data_preprocessing import GATE_TYPE_MAP
 
 
@@ -22,7 +23,7 @@ class QuantumCircuitGenerator(nn.Module):
         # Define models
         self.f_CNN = ConditioningCNN(self.hidden_dim)
         self.f_adder = Adder(self.node_feature_dim, self.hidden_dim)
-        self.f_gate_type = GatePredictor(self.node_feature_dim, self.hidden_dim)
+        self.f_gate_type = GatePredictor(self.node_feature_dim, self.hidden_dim, self.node_feature_dim)
         self.f_CNOT = self.build_CNOT_predictor()
         self.f_graph_RNN = self.build_graph_RNN()
         self.f_stopper = self.build_stopper()
@@ -71,14 +72,14 @@ class QuantumCircuitGenerator(nn.Module):
         """
         return nn.Linear(self.hidden_dim, 1)
 
-    def forward(self, x, qubo_data, window_size):
+    def forward(self, data, qubo_data, window_size):
         """
         Forward pass for generating a quantum circuit.
         :param graph_representation: Current state of the graph (e.g., node feature matrix)
         :param node_sequence: Sequence of nodes generated so far
         """
         # Unpack data tuples
-        X, A = x 
+        X, A = data.x, data.edge_index
         Q, num_qubits = qubo_data # to accomodate variable input sizes of qubo matrices, DataLoader will save the dimensions of QUBO matrices in num_qubits and then pad them dynamically to the maximum size in the batch
   
         stop_token = False
@@ -95,17 +96,23 @@ class QuantumCircuitGenerator(nn.Module):
         while not stop_token:
             layer = range(num_qubits)
             for qubit in layer:
-                alpha = self.f_adder(h, graph_state)
-                add = self.sample_add_action(alpha)
+                # alpha = self.f_adder(h, graph_state)
+                # add = self.sample_add_action(alpha)
 
-                if add:
-                    gamma = self.f_gate_type(h, graph_state)
-                    gate_type = self.sample_gate_type(gamma)
-                else:
-                    gate_type = 'I'
+                # if add:
+                #     gamma = self.f_gate_type(h, graph_state)
+                #     gate_type = self.sample_gate_type(gamma)
+                # else:
+                #     gate_type = 'i'
+
+                # GatePredictor can also predict the identity gate, so that there are no different control flows to handle
+                gamma = self.f_gate_type(h, graph_state)
+                gate_type = self.sample_gate_type(gamma)
 
                 X = self.update_node_features(X, gate_type)
                 A = self.update_adjacency(A, qubit)
+
+                data = Data(x=X, edge_index=A)
                 
                 if self.update_graph_at_each_addition:
                     graph_state = self.update_graph_state(X, A, window_size)
@@ -182,6 +189,49 @@ class QuantumCircuitGenerator(nn.Module):
         Placeholder for actual implementation.
         """
         return 'sample STOP'
+
+
+
+    def update_node_features(self, X, gate_type):
+        """
+        Update the node feature matrix.
+        It appends a new row (one-hot encoding of the gate type) to the node feature matrix, representing the new node added to the graph.
+
+        :param X: node feature matrix
+        :param gate_type: gate type to be added (str)
+
+        :return: updated node feature matrix
+        """
+        # X: (batch_size, num_nodes, node_feature_dim)
+        new_row = torch.zeros(X.size(0), 1, X.size(2)) 
+
+        # Get the index of the gate type in the mapping and set the corresponding element to 1
+        gate_idx = GATE_TYPE_MAP[gate_type]
+        new_row[:, :, gate_idx] = 1
+
+        return torch.cat((X, new_row), dim=1)
+
+
+
+    def update_adjacency(self, A, qubit, curr_layer, num_qubits):
+        """
+        Update the adjacency matrix.
+        In PyTorch Geometric, the adjacency matrix is represented in COO format, i.e. a tensor with shape (2, num_edges), that is num_edges
+        tuples of the form (source, target). To update the adjacency matrix, we need to add a new edge from the qubit-th node in the previous
+        layer to the new node in the graph.
+
+        :param A: adjacency matrix (COO format)
+        :param qubit: qubit on which the gate is applied
+
+        :return: updated adjacency matrix
+        """
+        # A: (batch_size, 2, num_edges)
+        # Add an edge from the qubit-th node in the previous layer to the new node (qubit-th node in the current layer)
+        idx_curr_node = A.size(2) # the index of new node is the number of edges in the graph
+        idx_prev_node = (curr_layer - 1) * num_qubits + qubit
+       
+        new_edge = torch.tensor([[idx_prev_node, idx_curr_node]], dtype=torch.long)
+        return torch.cat((A, new_edge), dim=2)
 
 
 
