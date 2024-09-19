abb4238c2b36a2f64c229284bddcb7348549fa57
melaniasala <melania.sala@mail.polimi.it>
2024-09-19 18:13:15 +0200

    Fix imports

--- QCCL/utils/__init__.py
+++ QCCL/utils/__init__.py
@@ -1,5 +1,6 @@
 from .train import train, train_byol
 from .losses import NTXentLoss
+from .HyperparamTuner import HyperparamTuner
 #from .evaluation import evaluate
 
-__all__ = ['train', 'NTXentLoss', 'train_byol']
+__all__ = ['train', 'NTXentLoss', 'train_byol', 'HyperparamTuner']
