a9e68221a47d0ff008659d57ace3a7b252b98031
melaniasala <melania.sala@mail.polimi.it>
2024-09-20 19:52:17 +0200

    Add dataset splitting function to utils.py

--- QCCL/Data/__init__.py
+++ QCCL/Data/__init__.py
@@ -1,4 +1,6 @@
 from .Dataset import GraphDataset
 from .transforms import Compose, perform_random_transform
 from .load_data import load_graphs
-__all__ = ['GraphDataset', 'Compose', 'perform_random_transform', 'load_graphs']
+from .utils import split_dataset
+
+__all__ = ['GraphDataset', 'Compose', 'perform_random_transform', 'load_graphs', 'split_dataset']
