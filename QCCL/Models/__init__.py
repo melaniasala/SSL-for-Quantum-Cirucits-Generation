481df0c1473f9f3231dcbcd16cbf2fc25d62d087
melaniasala <melania.sala@mail.polimi.it>
2024-09-20 19:53:02 +0200

    Add build_model function to utils.py

--- QCCL/Models/__init__.py
+++ QCCL/Models/__init__.py
@@ -1,6 +1,6 @@
 from .GNNFeatureExtractor import GNNFeatureExtractor, GCNFeatureExtractor
 from .models import SimCLR, BYOL, BYOLOnlineNet, BYOLTargetNet
-
+from .utils import build_model
 #from .ProjectionHead import ProjectionHead
 
 __all__ = ['GNNFeatureExtractor', 'GCNFeatureExtractor', 'SimCLR', 'BYOL', 'BYOLOnlineNet', 'BYOLTargetNet']
