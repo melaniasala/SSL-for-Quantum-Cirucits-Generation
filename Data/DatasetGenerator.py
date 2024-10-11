5d54a5989cc73e2e248e2fd27fbd4521274ed200
melaniasala <melania.sala@mail.polimi.it>
2024-10-11 12:36:57 +0200

    Refactor QuantumCircuitDataset class to save the dataset and visualize circuits and graphs

--- Data/DatasetGenerator.py
+++ Data/DatasetGenerator.py
@@ -1,9 +1,12 @@
+import json
+import pickle
 import random
+from math import ceil
 
+import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 from qiskit import Aer, execute
-from qiskit.quantum_info import Statevector
 
 from .QuantumCircuitGraph import QuantumCircuitGraph
 from .RandomCircuitGenerator import RandomCircuitGenerator
@@ -23,6 +26,7 @@ class QuantumCircuitDataset:
         self.dataset_size = dataset_size
         self.save_format = save_format
         self.dataset = []
+        self.qcg_dataset = []
         self.statevectors = {} 
 
         if graph_config is not None:
@@ -60,13 +64,12 @@ class QuantumCircuitDataset:
 
                 # Generate the circuit
                 circuit = self.circuit_generator.generate_circuit(num_qubits=num_qubits, depth=depth, max_gates=max_gates_value)
-
-                graph = QuantumCircuitGraph(circuit).graph
+                qcg = QuantumCircuitGraph(circuit)
                 
                 # Perform checks 
                 if self.check_equivalence(circuit) and self.check_connectivity(graph):
                     # If the circuit passes checks, add it to the dataset
-                    self.dataset.append((circuit, graph))
+                    self.dataset.append(qcg)
                     generated_count += 1
 
         print(f"Dataset generated with {len(self.dataset)} circuits.")
@@ -137,7 +140,7 @@ class QuantumCircuitDataset:
         """
         if strategy == 'exponential':
             # Placeholder for exponential function
-            return 2 ** num_qubits
+            return 2 ** (num_qubits-1)
         else:
             raise ValueError(f"Unknown max_gates strategy: {strategy}")
         
@@ -153,3 +156,112 @@ class QuantumCircuitDataset:
         QuantumCircuitGraph.set_include_identity_gates(graph_config.include_identity_gates)
         QuantumCircuitGraph.set_differentiate_cx(graph_config.differentiate_cx)
 
+    def save_dataset(self, file_path=None, include_statevectors=False):
+        """
+        Save the dataset of circuits and associated graphs to a file.
+        
+        Parameters:
+        file_path (str): Path where the dataset will be saved.
+        """
+        
+        if include_statevectors:
+            dataset_to_save = {
+                'dataset': self.dataset,
+                'statevectors': self.statevectors
+            }
+        else:
+            dataset_to_save = self.dataset
+
+        extension = 'json' if self.save_format == 'json' else 'pkl'
+
+        if file_path is None:
+            file_path = f"dataset_{self.dataset_size}.{extension}"
+        
+        try:
+            if self.save_format == 'json':
+                with open(file_path, 'w') as file:
+                    json.dump(dataset_to_save, file, indent=4)
+            elif self.save_format == 'pickle':
+                with open(file_path, 'wb') as file:
+                    pickle.dump(dataset_to_save, file)
+            
+            print(f"Dataset successfully saved to {file_path}")
+        
+        except Exception as e:
+            print(f"Failed to save dataset: {str(e)}")
+
+
+    def visualize_circuit(self, index=0, graph_visual_mode='circuit-like'):
+        """
+        Visualize a specific circuit or its corresponding graph from the dataset.
+
+        Parameters:
+        index (int): Index of the circuit in the dataset to visualize (default is 0, the first one).
+        graph_visual_mode (str): Mode for visualizing the graph. 
+            - 'circuit-like' (default): Visualizes the graph in a way that resembles the quantum circuit structure.
+            - 'graph': Visualizes the graph without the circuit-like layout.
+            - None: Visualizes the quantum circuit itself (Qiskit's circuit diagram).
+        
+        If the index is out of range, an error message will be displayed.
+        """
+        if index >= len(self.dataset):
+            print(f"Index {index} is out of range. There are {len(self.dataset)} circuits in the dataset.")
+            return
+        
+        if graph_visual_mode is None:
+            self.dataset[index].circuit.draw(output='mpl')
+
+        elif graph_visual_mode == 'circuit-like':
+            self.dataset[index].draw_circuit_like_graph(circuit_like_graph=True)
+
+        elif graph_visual_mode == 'graph':
+            self.dataset[index].draw_circuit_and_graph(circuit_like_graph=False)
+
+    def visualize_dataset(self, num_samples=12, graph_visual_mode='circuit-like'):
+        """
+        Visualize a sample of circuits and their corresponding graphs from the dataset.
+
+        Parameters:
+        num_samples (int): Number of circuits to visualize. Defaults to 12.
+        graph_visual_mode (str): Mode for visualizing the graph. 
+            - 'circuit-like' (default): Visualizes the graph to resemble the quantum circuit structure.
+            - 'graph': Visualizes the graph layout without the circuit-like appearance.
+            - None: Visualizes the quantum circuit itself (Qiskit's circuit diagram).
+        """
+        if not self.dataset:
+            print("No circuits have been generated yet. Please generate circuits first.")
+            return
+        
+        # Adjust number of samples if more requested than available
+        num_samples = min(num_samples, len(self.dataset))
+        visual_mode = True if graph_visual_mode == 'circuit-like' else False
+
+        if graph_visual_mode:
+            samples_per_row = 3
+            columns = 2 * samples_per_row  # Each sample gets 2 subplots (circuit and graph)
+        else:
+            samples_per_row = 5
+            columns = samples_per_row  # Only circuits are visualized
+
+        rows = ceil(num_samples / samples_per_row)
+        add_height = 2
+        fig, axs = plt.subplots(rows, columns, figsize=(15, num_samples + add_height*(1/(rows)**2)))
+
+        if rows == 1:
+            axs = np.expand_dims(axs, axis=0)
+        
+        for i in range(num_samples):
+            row = i // samples_per_row
+            col = i % samples_per_row
+            
+            if graph_visual_mode:
+                col_start = 2 * col  # Circuit on one, graph on the next
+                self.dataset[i].draw_circuit_and_graph(circuit_like_graph=visual_mode, axs=axs[row, col_start:col_start + 2])
+            else:
+                # Circuit-only visualization
+                self.dataset[i].circuit.draw(output='mpl', ax=axs[row, col])
+
+
+            plt.tight_layout()
+            plt.axis('off')
+            plt.show()
