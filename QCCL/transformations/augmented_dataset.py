2242c1e3e1e681e24034bae46cd6299260ae8362
melaniasala <melania.sala@mail.polimi.it>
2025-01-06 12:01:25 +0100

    Save each sample to separate file; implement unique identifiers and metadata

--- QCCL/transformations/augmented_dataset.py
+++ QCCL/transformations/augmented_dataset.py
@@ -40,9 +40,10 @@ def generate_augmented_dataset(input_file, transformations=None, save_interval=1
 
     # Load dataset in chunks if specified
     if chunk_size:
-        dataset = data["dataset"][:chunk_size]
+        dataset = data["dataset"][:chunk_size].copy()
     else:
-        dataset = data["dataset"]
+        dataset = data["dataset"].copy()
+    del data
 
     # Create output directory if it doesn't exist
     if not output_dir:
@@ -66,22 +67,15 @@ def generate_augmented_dataset(input_file, transformations=None, save_interval=1
                 "num_qubits": n_qubits,
                 "num_samples": 0,
                 "num_views": 0,
+                "transformations": transformations,
                 "timestamp": datetime.now().isoformat(),
-                "statevector_dir": statevectors_dir
             },
             "logs": {
                 "processed_count": 0,
-                "samples": {}
+                "processed_samples": {}
             },
-            "configuration": {
-                "transformations": transformations,
-                "save_interval": save_interval
-            }
+            "samples": {}
         }
-
-    # Load or initialize augmented dataset
-    data_file = f"augmented_dataset_{n_qubits}_qubits.pkl"
-    data_path = os.path.join(output_dir, data_file) if output_dir else data_file
 
     # Process samples
     for idx in range(dataset_size):
@@ -91,54 +85,77 @@ def generate_augmented_dataset(input_file, transformations=None, save_interval=1
             chunk_end = min(idx + chunk_size, dataset_size)
             print("-" * 80)
             print(f"Loading chunk {idx//chunk_size + 1}/{dataset_size//chunk_size} (from sample {chunk_start} to {chunk_end})...")
-            dataset = data["dataset"][chunk_start:chunk_end]
+            with open(input_file, 'rb') as f:
+                data = pickle.load(f)
+            dataset = data["dataset"][chunk_start:chunk_end].copy()
+            del data
         sample = dataset[idx%chunk_size] if chunk_size else dataset[idx]
 
+        sample_id = f"q{n_qubits}_s{idx:02}"
         print("-" * 80)
-        print(f"Processing sample {idx+1}/{dataset_size} (sample_id: {idx})...")
-        if str(idx) in metadata["logs"]["samples"]:
-            print(f"Sample {idx} already processed. Skipping...")
+        print(f"Processing sample {idx+1}/{dataset_size} (sample_id: {sample_id})...")
+        if sample_id in metadata["samples"]:
+            print(f"Sample {sample_id} already processed. Skipping...")
+            # Delete statevector file if it exists
+            if os.path.exists(os.path.join(statevectors_dir, f"statevector_{idx}.npz")):
+                os.remove(os.path.join(statevectors_dir, f"statevector_{idx}.npz"))
             continue
         else:
             print()
 
-        # Initialize sample log and views
-        metadata["logs"]["samples"][f"{idx}"] = {"successfully_applied": [], "successful_count": 0}
-        sample_views = {}
+         # Create folder for the sample
+        sample_dir = os.path.join(output_dir, sample_id)
+        os.makedirs(sample_dir, exist_ok=True)
+
+        # Initialize sample logs
+        metadata["logs"]["processed_samples"][f"{idx}"] = {"successfully_applied": [], "successful_count": 0}
+        # Initialize sample metadata
+        metadata["samples"][sample_id] = {
+            "statevector_file": os.path.join(sample_dir, f"{sample_id}_statevector.npz"),
+            "views": {}
+        }
+
+        # Save statevector 
+        statevector_path = os.path.join(statevectors_dir, f"statevector_{idx}.npz")
+        with np.load(statevector_path, allow_pickle=True) as sv:
+            sample_statevector = sv
+        statevector_file = os.path.join(sample_dir, f"{sample_id}_statevector.npz")
+        np.savez_compressed(statevector_file, sample_statevector)
+        metadata["samples"][sample_id]["statevector_file"] = statevector_file
+
+        # Free memory
+        del sample_statevector
+        os.remove(statevector_path)
 
         # Save base sample
-        sample_views[0] = {
-            "graph": sample.graph,
-            "quantum_circuit": sample.quantum_circuit,
+        view_idx = 0
+        view_id = f"{sample_id}_v{view_idx:03}"
+        base_view_filename = f"{view_id}.pkl"
+        base_view_path = os.path.join(sample_dir, base_view_filename)
+        with open(base_view_path, "wb") as f:
+            pickle.dump({
+                "graph": sample.graph,
+                "quantum_circuit": sample.quantum_circuit,
+                "transformations": []
+            }, f)
+        metadata["samples"][sample_id]["views"][view_id] = {
+            "file": base_view_path,
             "transformations": []
         }
         metadata["dataset_info"]["num_views"] += 1
 
         # Apply transformations
-        successful_transformations = augment(sample, transformations, sample_views)
-
-        # Save statevector and views to the augmented dataset, and free memory
-        statevector_path = os.path.join(statevectors_dir, f"statevector_{idx}.npz")
-        with np.load(statevector_path, allow_pickle=True) as sv:
-            sample_data = {
-                "sample_id": idx,
-                "statevector": sv,
-                "views": sample_views
-            }
-
-        # Delete the statevector file to free up memory
-        os.remove(statevector_path)
+        successful_transformations = augment(sample, transformations, sample_id, sample_dir, metadata)
 
         # Update logs in metadata
-        metadata["logs"]["samples"][f"{idx}"]["successfully_applied"] = successful_transformations
-        metadata["logs"]["samples"][f"{idx}"]["successful_count"] += len(successful_transformations)
+        metadata["logs"]["processed_samples"][f"{idx}"]["successfully_applied"] = successful_transformations
+        metadata["logs"]["processed_samples"][f"{idx}"]["successful_count"] += len(successful_transformations)
         metadata["logs"]["processed_count"] += 1
         metadata["dataset_info"]["num_samples"] += 1
-        metadata["dataset_info"]["num_views"] += len(successful_transformations)
 
         # Save progress periodically
         if idx % save_interval == 0:
-            save_progress(data_path, sample_data, metadata_path, metadata)
+            save_metadata(metadata_path, metadata)
 
     print("Augmented dataset generation completed.")
 
@@ -152,13 +169,6 @@ def generate_augmented_dataset(input_file, transformations=None, save_interval=1
 
 def save_statevectors(statevectors, n_qubits, directory):
     """Save statevectors as a compressed .npz file and separate .npy files."""
-    # Save all statevectors as a single compressed .npz file
-    filename = f"statevectors_{n_qubits}_qubits.npz" 
-    file_path = os.path.join(directory, filename) if directory else filename
-    reduced_statevectors = [np.array(statevector, dtype=np.float16) for statevector in statevectors]
-    np.savez_compressed(file_path, *reduced_statevectors)
-    print(f"Statevectors saved to file {file_path} (compressed)")
-
     # Save individual statevectors as .npy files in a directory
     directory = os.path.join(directory, f"statevectors_{n_qubits}_qubits") if directory else f"statevectors_{n_qubits}_qubits"
     os.makedirs(directory, exist_ok=True)
@@ -168,12 +178,12 @@ def save_statevectors(statevectors, n_qubits, directory):
     print(f"Statevectors saved separately to directory {directory}\n")
 
     # Free up memory
-    del statevectors, compressed_statevector, reduced_statevectors, sv
+    del statevectors, compressed_statevector, sv
 
     return directory
 
 
-def augment(sample, transformations, dictionary):
+def augment(sample, transformations, sample_id, sample_dir, metadata):
     """Apply transformations to a sample and log results."""
     successful_transformations = {}
     view_idx = 1
@@ -183,12 +193,21 @@ def augment(sample, transformations, dictionary):
         try:
             with contextlib.redirect_stdout(StringIO()), contextlib.redirect_stderr(StringIO()):
                 transformed = CompositeTransformation(sample, [t_1]).apply()
-            dictionary[view_idx] = {
-                "graph": transformed.graph,
-                "circuit": transformed.quantum_circuit,
+            view_id = f"{sample_id}_v{view_idx:03}"
+            view_filename = f"{view_id}.pkl"
+            view_path = os.path.join(sample_dir, view_filename)
+            with open(view_path, "wb") as f:
+                pickle.dump({
+                    "graph": transformed.graph,
+                    "quantum_circuit": transformed.quantum_circuit,
+                    "transformations": [t_1]
+                }, f)
+            metadata["samples"][sample_id]["views"][view_id] = {
+                "file": view_path,
                 "transformations": [t_1]
             }
-            successful_transformations[f"{view_idx}"] = t_1
+            metadata["dataset_info"]["num_views"] += 1
+            successful_transformations[f"{view_id}"] = t_1
             view_idx += 1
         except NoMatchingSubgraphsError:
             unsuc_log += f"No matching subgraphs found for {t_1}: moving on to the next transformation.\n"
@@ -201,12 +220,21 @@ def augment(sample, transformations, dictionary):
             try:
                 with contextlib.redirect_stdout(StringIO()), contextlib.redirect_stderr(StringIO()):
                     transformed = CompositeTransformation(sample, [t_1, t_2]).apply()
-                dictionary[view_idx] = {
-                    "graph": transformed.graph,
-                    "circuit": transformed.quantum_circuit,
+                view_id = f"{sample_id}_v{view_idx:03}"
+                view_filename = f"{view_id}.pkl"
+                view_path = os.path.join(sample_dir, view_filename)
+                with open(view_path, "wb") as f:
+                    pickle.dump({
+                        "graph": transformed.graph,
+                        "quantum_circuit": transformed.quantum_circuit,
+                        "transformations": [t_1, t_2]
+                    }, f)
+                metadata["samples"][sample_id]["views"][view_id] = {
+                    "file": view_path,
                     "transformations": [t_1, t_2]
                 }
-                successful_transformations[f"{view_idx}"] = f"{t_1}, {t_2}"
+                metadata["dataset_info"]["num_views"] += 1
+                successful_transformations[f"{view_id}"] = [t_1, t_2]
                 view_idx += 1
             except NoMatchingSubgraphsError:
                 unsuc_log += f"No matching subgraphs found for {t_1}, {t_2}: moving on to the next transformation.\n"
@@ -221,33 +249,21 @@ def augment(sample, transformations, dictionary):
     return successful_transformations
 
 
-def save_progress(file_path, data_to_append, metadata_path, metadata):
-    """Save the augmented dataset and progress log."""
-
-    # Use a temporary file for safer incremental saving
-    temp_file_path = file_path.replace('.pkl', '.tmp')
+def save_metadata(metadata_path, metadata):
+    """Save metadata to a JSON file."""
 
     try:
         print("\nSaving progress...")
-        append_sample_to_file(file_path, data_to_append)
-        print(f"Saved {file_path} successfully.")
         save_to_file(metadata_path, metadata, '.json')
 
-        print(f"Both {file_path} and {metadata_path} saved successfully.\n")
+        print(f"Metadata saved to {metadata_path} successfully.\n")
 
         return True
 
     except Exception as e:
-        # Cleanup in case of failure
-        print(f"Error: Failed to save files. Exception details: {str(e)}")
+        print(f"Error: Failed to save file. Exception details: {str(e)}")
         print(traceback.format_exc())
-
-        if os.path.exists(temp_file_path):
-            os.remove(temp_file_path)
-        if os.path.exists(file_path):
-            os.remove(file_path)
-        if os.path.exists(metadata_path):
-            os.remove(metadata_path)
+        # Stop execution and raise the exception
         raise e
 
 def save_to_file(file_path, data, format=None, temp_file_path=None):
@@ -281,22 +297,3 @@ def save_to_file(file_path, data, format=None, temp_file_path=None):
         # Stop execution and raise the exception
         raise e
     
-
-def append_sample_to_file(file_path, sample):
-    """Append a single sample to the dataset file incrementally."""
-    try:
-        if not os.path.exists(file_path):
-            # Create a new file if it doesn't exist
-            with open(file_path, "wb") as f:
-                pickle.dump([sample], f)
-        else:
-            # Append to the existing file
-            with open(file_path, "rb") as f:
-                existing_data = pickle.load(f)
-            existing_data.append(sample)
-            with open(file_path, "wb") as f:
-                pickle.dump(existing_data, f)
-    except Exception as e:
-        print(f"Error while appending sample to file: {e}")
-        raise
-
